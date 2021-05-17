ed3816718f0bee5ab221e9924f3618cd0195d854
1esolano <83794793+1esolano@users.noreply.github.com>
2021-05-16 18:56:25 -0600

    tarea3.2
    tarea tres actualizada

--- tarea 3 MRL en R.R
+++ tarea 3 MRL en R.R
@@ -7,11 +7,8 @@ autos_filtrado = select(Base_Auto,Año_Auto,Precio_Auto)
 
 
 View(cor(autos_filtrado))
-
 pairs(autos_filtrado)
-
 str(autos_filtrado)
-
 attach(autos_filtrado)
 
 lm_autos = lm(Precio_Auto~Año_Auto)
@@ -24,5 +21,5 @@ pred_precio = data.frame(Año_Auto = c(2005L,1998L,2001L,2011L,1988L))
 predict(lm_autos,pred_precio)
 
 
-  
+
 
